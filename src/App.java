39a640a4c09315ce1f43f85050e946a53ed8ac40
noaho <noaho@NoahsLaptop>
2022-12-15 13:36:49 +0100

    hghfg

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Window w = new Window();
+        System.out.println("");
     }
 }
