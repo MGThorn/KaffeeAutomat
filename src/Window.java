6356ac3f60c7d07c03c6818c68d0492423c9736e
MGThorn <107687299+MGThorn@users.noreply.github.com>
2022-12-15 12:10:00 +0100

    tasse lbl

--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.AttributeSet.ColorAttribute;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,7 +17,7 @@ public class Window
   Window() throws IOException{
     frame = new JFrame();
     dim = Toolkit.getDefaultToolkit().getScreenSize();
-    background = ImageIO.read(new File("rsc\\R.png"));
+    background = ImageIO.read(new File("rsc\\background.jpg"));
     
     //init frame
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -30,8 +32,22 @@ public class Window
           g.drawImage(background, 0, 0,((int)dim.getWidth()/2),((int)dim.getWidth()/2), null);
       }
     };
+    pane.setLayout(null);
     //add background to Frame
     frame.getContentPane().add(pane, BorderLayout.CENTER);
+
+    JLabel monitor = new JLabel();
+    monitor.setBounds(((int)(dim.getWidth()/4.8)),((int)(dim.getHeight()/3.1*1.7)),100,100);
+    monitor.setVisible(true);
+    monitor.setOpaque(false);
+    monitor.setText("null");
+    pane.add(monitor);
+
+    JButton button = new JButton();
+    button.setBounds(0,0,10,10);
+    button.setVisible(true);
+    button.setOpaque(true);
+    pane.add(button);
 
   }
 }
