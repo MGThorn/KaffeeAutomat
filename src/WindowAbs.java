98eae11b66988c46762aa24758c1985f99104198
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-01-18 14:08:46 +0100

    maaaan

--- src/WindowAbs.java
+++ src/WindowAbs.java
@@ -46,26 +46,56 @@ public class WindowAbs
     cup.setIcon(new ImageIcon(this.cup_img));
     pane.add(cup);
 
+    JButton onButton = new JButton();
+    onButton.setBounds(250,500,50,50);
+    onButton.setVisible(true);
+    onButton.setIcon(new ImageIcon(this.cup_img));
+    onButton.setOpaque(true);
+    pane.add(onButton);
+
     JButton button1 = new JButton();
-    button1.setBounds(300,170,50,50);
+    button1.setBounds(300,185,50,50);
     button1.setVisible(true);
     button1.setIcon(new ImageIcon(this.cup_img));
     button1.setOpaque(true);
     pane.add(button1);
 
     JButton button2 = new JButton();
-    button2.setBounds(250,170,50,50);
+    button2.setBounds(250,185,50,50);
     button2.setVisible(true);
     button2.setIcon( new ImageIcon(this.cup_img));
     button2.setOpaque(true);
     pane.add(button2);
 
     JButton button3 = new JButton();
-    button3.setBounds(200,170,50,50);
+    button3.setBounds(200,185,50,50);
     button3.setVisible(true);
     button3.setIcon(new ImageIcon(this.cup_img));
     button3.setOpaque(true);
     pane.add(button3);
+
+    JButton milkButton = new JButton();
+    milkButton.setBounds(200,185,50,50);
+    milkButton.setVisible(true);
+    milkButton.setIcon(new ImageIcon(this.cup_img));
+    milkButton.setOpaque(true);
+    pane.add(milkButton);
+
+    JButton coffeeButton = new JButton();
+    coffeeButton.setBounds(200,185,50,50);
+    coffeeButton.setVisible(true);
+    coffeeButton.setIcon(new ImageIcon(this.cup_img));
+    coffeeButton.setOpaque(true);
+    pane.add(coffeeButton);
+
+
+
+    JLabel txtOutput = new JLabel();
+    txtOutput.setBounds(210,165,100,25);
+    txtOutput.setText("null");
+    txtOutput.setVisible(true);
+    txtOutput.setOpaque(true);
+    pane.add(txtOutput);
 
   }
 }
