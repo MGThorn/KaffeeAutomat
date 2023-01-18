e2b1fc6ee986e9ef1111a6768b3a2c19d2e834c6
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-01-18 15:19:13 +0100

    gui changes

--- src/WindowAbs.java
+++ src/WindowAbs.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import javax.swing.text.AttributeSet.ColorAttribute;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,18 +13,27 @@ public class WindowAbs
   JFrame frame;
   JPanel pane;
   Dimension dim;
+
   Image background;
   Image cup_img;
+  Image milk_img;
+  Image coffee_img;
+  Image water_img;
+
+  int zustand;
 
   WindowAbs() throws IOException{
     frame = new JFrame();
     dim = Toolkit.getDefaultToolkit().getScreenSize();
     background = ImageIO.read(new File("rsc\\background.jpg"));
     cup_img = ImageIO.read(new File("rsc\\Cup.png"));
+    milk_img = ImageIO.read(new File("rsc\\Milk.jfif"));
+    coffee_img = ImageIO.read(new File("rsc\\coffee.jfif"));
+    water_img = ImageIO.read(new File("rsc\\Water.jfif"));
     
     //init frame
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    frame.setBounds(0, 0, 600, 600);
+    frame.setBounds(0, 0, 900, 600);
     frame.setVisible(true);
 
     //init Background
@@ -37,6 +47,14 @@ public class WindowAbs
     pane.setLayout(null);
     //add background to Frame
     frame.getContentPane().add(pane, BorderLayout.CENTER);
+
+    
+    JLabel txtOutput = new JLabel();
+    txtOutput.setBounds(210,165,150,25);
+    txtOutput.setText("null");
+    txtOutput.setVisible(true);
+    txtOutput.setOpaque(true);
+    pane.add(txtOutput);
 
     JLabel cup = new JLabel();
     cup.setBounds(200,300,200,200);
@@ -75,27 +93,49 @@ public class WindowAbs
     pane.add(button3);
 
     JButton milkButton = new JButton();
-    milkButton.setBounds(200,185,50,50);
+    milkButton.setBounds(550,185,70,300);
     milkButton.setVisible(true);
-    milkButton.setIcon(new ImageIcon(this.cup_img));
-    milkButton.setOpaque(true);
+    milkButton.setIcon(new ImageIcon(this.milk_img));
+    milkButton.setOpaque(false);
+    milkButton.setBorderPainted(false);
+    milkButton.setContentAreaFilled(false);
+    milkButton.setFocusable(false);
+    milkButton.addActionListener(new ActionListener(){  
+      public void actionPerformed(ActionEvent e){  
+              txtOutput.setText("Welcome to Javatpoint.");  
+      }  
+      });  
     pane.add(milkButton);
 
     JButton coffeeButton = new JButton();
-    coffeeButton.setBounds(200,185,50,50);
+    coffeeButton.setBounds(650,185,70,300);
     coffeeButton.setVisible(true);
-    coffeeButton.setIcon(new ImageIcon(this.cup_img));
+    coffeeButton.setIcon(new ImageIcon(this.coffee_img));
     coffeeButton.setOpaque(true);
+    coffeeButton.setBorderPainted(false);
+    coffeeButton.setContentAreaFilled(false);
+    coffeeButton.setFocusable(false);
+    coffeeButton.addActionListener(new ActionListener(){  
+      public void actionPerformed(ActionEvent e){  
+              txtOutput.setText("Welcome to Javatpoint.");  
+      }  
+      }); 
     pane.add(coffeeButton);
 
-
-
-    JLabel txtOutput = new JLabel();
-    txtOutput.setBounds(210,165,100,25);
-    txtOutput.setText("null");
-    txtOutput.setVisible(true);
-    txtOutput.setOpaque(true);
-    pane.add(txtOutput);
+    JButton waterButton = new JButton();
+    waterButton.setBounds(750,185,70,300);
+    waterButton.setVisible(true);
+    waterButton.setIcon(new ImageIcon(this.water_img));
+    waterButton.setOpaque(true);
+    waterButton.setBorderPainted(false);
+    waterButton.setContentAreaFilled(false);
+    waterButton.setFocusable(false);
+    waterButton.addActionListener(new ActionListener(){  
+      public void actionPerformed(ActionEvent e){  
+              txtOutput.setText("Welcome to Javatpoint.");  
+      }  
+      }); 
+    pane.add(waterButton);
 
   }
 }
